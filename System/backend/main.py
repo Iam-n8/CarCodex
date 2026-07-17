542ac320f858cececf968240ca1e333d728c69be
Iam-n8 <nbrick@gmail.com>
2026-07-17 08:18:53 -0500

    Add Jinja2 dashboard with live database metrics

--- System/backend/main.py
+++ System/backend/main.py
@@ -3,23 +3,30 @@
 # import os
 from routers import vehicles
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
 from database import engine
+from database import SessionLocal
+
 from routers import mileage
 from routers import events
 from routers import documents
 from routers import costs
 from routers import snapshots
-from models import Base
+from models  import Base
 from routers import uploads
 from routers import maintenance
 from routers import maintenance_schedule
 from routers import maintenance_visit
 from routers import vendor
-
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
-
+from models import (
+    Vehicle,
+    Vendor,
+    MaintenanceVisit,
+    Document
+)
 
 
 # --------------------------------------------------
@@ -57,6 +64,10 @@ Base.metadata.create_all(bind=engine)
 # Root
 # --------------------------------------------------
 
+# --------------------------------------------------
+# dashboard
+# --------------------------------------------------
+
 @app.get(
     "/ui",
     response_class=HTMLResponse
@@ -65,12 +76,26 @@ def dashboard(
     request: Request
 ):
 
+    db = SessionLocal()
+
+    vehicle_count = db.query(Vehicle).count()
+
+    vendor_count = db.query(Vendor).count()
+
+    visit_count = db.query(MaintenanceVisit).count()
+
+    document_count = db.query(Document).count()
+
+    db.close()
+
     return templates.TemplateResponse(
         request=request,
         name="dashboard.html",
         context={
-            "request": request
+            "request": request,
+            "vehicle_count": vehicle_count,
+            "vendor_count": vendor_count,
+            "visit_count": visit_count,
+            "document_count": document_count
         }
     )
-
-
